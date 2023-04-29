8af9ee5a5d5d1bf57b558d233454087a0ed98c8e
mashanstrk <117375259+mashanstrk@users.noreply.github.com>
2023-04-29 21:26:40 +0000

    Deploying to gh-pages from @ mashanstrk/goit-js-hw-08@3f6ec128fd008fdf291c6c11838f981fa3ce9a3d 🚀

---
+++ 01-gallery.1bb7fbb1.js
@@ -0,0 +1,2 @@
+console.log("galleryItems"),mashanstrk;
+//# sourceMappingURL=01-gallery.1bb7fbb1.js.map
---
+++ 01-gallery.4b2b28e8.js
@@ -0,0 +1,2 @@
+console.log("galleryItems"),mashanstrk;
+//# sourceMappingURL=01-gallery.4b2b28e8.js.map
