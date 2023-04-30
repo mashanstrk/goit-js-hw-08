294033eb201a53fd74b145697076679bdd4cb97f
mashanstrk <117375259+mashanstrk@users.noreply.github.com>
2023-04-30 14:56:55 +0300

    task1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log("galleryItems"); mashanstrk
+console.log(galleryItems); 
+
+const gallery = document.querySelector(".gallery");
+
+console.log(gallery);
+
+const markup = galleryItems.map(({preview, original, description}) => `<div class="gallery__item">
+<a class="gallery__link" href=${original}>
+   <img class="gallery__image" src=${preview} alt=${description} />
+</a>
+</div>`).join('');
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+
+// Додавання модального вікна
+
+// const lightbox = new SimpleLightbox('.gallery a');
+
+new SimpleLightbox('.gallery a', {
+    captionsData:"alt",
+    captionDelay: 250,
+});
